b4fc83fa4f1c2ea741e5db4946d00721be24152f
amadejpapez <amadej.papez@icloud.com>
2023-12-07 10:20:21 +0100

    ref: add CellActor and CellState

---
+++ core/src/com/amadejpapez/bombit/CellActor.java
@@ -0,0 +1,26 @@
+package com.amadejpapez.bombit;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+
+public class CellActor extends Image {
+    private CellState state;
+
+    public CellActor(TextureRegion region) {
+        super(region);
+        state = CellState.EMPTY;
+    }
+
+    public void setState(CellState state) {
+        this.state = state;
+    }
+
+    public void setDrawable(TextureRegion region) {
+        super.setDrawable(new TextureRegionDrawable(region));
+    }
+
+    public boolean isEmpty() {
+        return state == CellState.EMPTY;
+    }
+}
---
+++ core/src/com/amadejpapez/bombit/CellState.java
@@ -0,0 +1,3 @@
+package com.amadejpapez.bombit;
+
+public enum CellState {EMPTY, BASKET_BLUE, BASKET_ORANGE, CONE_RED, CONE_BLUE, POM_PINK, POM_YELLOW, FLOOR, MIDDLE}
