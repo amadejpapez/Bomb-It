a13b9f4ce9f42f736455a100b9823a7d84d6445c
amadejpapez <amadej.papez@icloud.com>
2023-12-07 10:17:00 +0100

    feat: add floor image

--- core/src/com/amadejpapez/bombit/assets/AssetRegionNames.java
+++ core/src/com/amadejpapez/bombit/assets/AssetRegionNames.java
@@ -24,6 +24,7 @@ public class AssetRegionNames {
     public static final String YOU_WIN = "you_win";
     public static final String YOU_LOSE = "you_lose";
 
+    public static final String FLOOR = "floor";
     public static final String MIDDLE = "middle";
     public static final String BASKET_BLUE = "basket_blue";
     public static final String BASKET_ORANGE = "basket_orange";
