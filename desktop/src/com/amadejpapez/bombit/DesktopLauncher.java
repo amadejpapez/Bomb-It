8f5c54df1d3b04d326c8bbe3e637fa8aac291221
amadejpapez <amadej.papez@icloud.com>
2023-12-07 02:51:39 +0100

    ref: add GameConfig

--- desktop/src/com/amadejpapez/bombit/DesktopLauncher.java
+++ desktop/src/com/amadejpapez/bombit/DesktopLauncher.java
@@ -1,5 +1,6 @@
 package com.amadejpapez.bombit;
 
+import com.amadejpapez.bombit.config.GameConfig;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
@@ -9,6 +10,7 @@ public class DesktopLauncher {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
 		config.setForegroundFPS(60);
 		config.setTitle("Bomb It 4");
+		config.setWindowedMode((int) GameConfig.WIDTH, (int) GameConfig.HEIGHT);
 		new Lwjgl3Application(new BombIt(), config);
 	}
 }
