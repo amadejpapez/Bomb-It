2fff245ece21cba245fb087254a0dc8e2e41c429
amadejpapez <amadej.papez@icloud.com>
2023-12-12 22:52:22 +0100

    style: moving around and better code formatting

--- core/src/com/amadejpapez/bombit/config/GameConfig.java
+++ core/src/com/amadejpapez/bombit/config/GameConfig.java
@@ -1,5 +1,8 @@
 package com.amadejpapez.bombit.config;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Be careful which 'GameConfig' class you use in a specific examples. This class is 'public' because
  * of demonstration purposes. In this way, you can use it in all packages inside gameplay and also in
@@ -16,6 +19,71 @@ public class GameConfig {
 
     public static final float WORLD_WIDTH = 80f;    // world units
     public static final float WORLD_HEIGHT = 60f;   // world units
+
+    public static final Integer NUM_COLUMNS = 17;
+    public static final Integer NUM_ROWS = 15;
+    public static final Integer CELL_SIZE = 4;
+
+    public static final List<Integer> PLAYER1_START = List.of(1, 1);
+
+    public static final List<List<Integer>> LOC_FIXED_OBSTACLES = List.<List<Integer>>of(
+            List.of(1, 11),
+            List.of(10, 15),
+            List.of(10, 7),
+            List.of(10, 8),
+            List.of(10, 9),
+            List.of(11, 11),
+            List.of(11, 13),
+            List.of(11, 3),
+            List.of(11, 5),
+            List.of(12, 10),
+            List.of(12, 11),
+            List.of(12, 13),
+            List.of(12, 14),
+            List.of(12, 2),
+            List.of(12, 3),
+            List.of(12, 5),
+            List.of(12, 6),
+            List.of(12, 8),
+            List.of(13, 5),
+            List.of(2, 10),
+            List.of(2, 11),
+            List.of(2, 13),
+            List.of(2, 14),
+            List.of(2, 2),
+            List.of(2, 3),
+            List.of(2, 5),
+            List.of(2, 6),
+            List.of(2, 8),
+            List.of(3, 11),
+            List.of(3, 13),
+            List.of(3, 3),
+            List.of(3, 5),
+            List.of(4, 1),
+            List.of(4, 7),
+            List.of(4, 8),
+            List.of(4, 9),
+            List.of(5, 11),
+            List.of(5, 13),
+            List.of(5, 3),
+            List.of(5, 5),
+            List.of(6, 11),
+            List.of(6, 13),
+            List.of(6, 14),
+            List.of(6, 2),
+            List.of(6, 3),
+            List.of(6, 5),
+            List.of(8, 11),
+            List.of(8, 13),
+            List.of(8, 14),
+            List.of(8, 2),
+            List.of(8, 3),
+            List.of(8, 5),
+            List.of(9, 11),
+            List.of(9, 13),
+            List.of(9, 3),
+            List.of(9, 5)
+    );
 
     private GameConfig() {
     }
--- core/src/com/amadejpapez/bombit/screen/GameScreen.java
+++ core/src/com/amadejpapez/bombit/screen/GameScreen.java
@@ -1,6 +1,5 @@
 package com.amadejpapez.bombit.screen;
 
-import com.amadejpapez.bombit.GameManager;
 import com.amadejpapez.bombit.assets.Assets;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
@@ -13,7 +12,6 @@ import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
-import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -27,7 +25,6 @@ import java.util.concurrent.ThreadLocalRandom;
 import com.amadejpapez.bombit.assets.AssetDescriptors;
 import com.amadejpapez.bombit.assets.AssetRegionNames;
 import com.amadejpapez.bombit.CellActor;
-import com.amadejpapez.bombit.CellState;
 import com.amadejpapez.bombit.BombIt;
 import com.amadejpapez.bombit.config.GameConfig;
 
@@ -40,19 +37,18 @@ public class GameScreen extends ScreenAdapter {
     private Stage gameplayStage;
     private Stage hudStage;
 
-    private Skin skin;
-    private TextureAtlas gameplayAtlas;
+    private final TextureAtlas gameplayAtlas = Assets.assetManager.get(AssetDescriptors.GAMEPLAY);
 
-    private final CellState move = GameManager.INSTANCE.getInitMove();
-    private Image infoImage;
-
-    private final Integer NUM_COLUMNS = 17;
-    private final Integer NUM_ROWS = 15;
-    private final Integer CELL_SIZE = 4;
+    final TextureRegion coneRed = gameplayAtlas.findRegion(AssetRegionNames.CONE_RED);
+    final TextureRegion playerBlue = gameplayAtlas.findRegion(AssetRegionNames.PLAYER_BLUE);
+    final TextureRegion basketBlue = gameplayAtlas.findRegion(AssetRegionNames.BASKET_BLUE);
+    final TextureRegion basketOrange = gameplayAtlas.findRegion(AssetRegionNames.BASKET_ORANGE);
+    final TextureRegion empty = gameplayAtlas.findRegion(AssetRegionNames.EMPTY);
+    final TextureRegion floor = gameplayAtlas.findRegion(AssetRegionNames.FLOOR);
 
     private final List<List<CellActor>> cells = new ArrayList<>();
 
-    private final List<Integer> playerPosition = new ArrayList<>();
+    private final List<Integer> playerPosition = new ArrayList<>(GameConfig.PLAYER1_START);
 
     public GameScreen(BombIt game) {
         this.game = game;
@@ -66,12 +62,9 @@ public class GameScreen extends ScreenAdapter {
         gameplayStage = new Stage(viewport, game.getBatch());
         hudStage = new Stage(hudViewport, game.getBatch());
 
-        skin = Assets.assetManager.get(AssetDescriptors.SKIN);
-        gameplayAtlas = Assets.assetManager.get(AssetDescriptors.GAMEPLAY);
-
         gameplayStage.addActor(createGridBackground());
-        gameplayStage.addActor(createGrid());
-        gameplayStage.addActor(createGridMiddle());
+        gameplayStage.addActor(createGridMain());
+        gameplayStage.addActor(createMiddle());
 
         Gdx.input.setInputProcessor(new InputMultiplexer(gameplayStage, hudStage));
     }
@@ -109,10 +102,10 @@ public class GameScreen extends ScreenAdapter {
     private Actor createGridBackground() {
         Table bgTable = new Table();
         Table bgGrid = new Table();
-        bgGrid.defaults().size(CELL_SIZE);
+        bgGrid.defaults().size(GameConfig.CELL_SIZE);
 
-        for (int i = 0; i < NUM_ROWS; i++) {
-            for (int j = 0; j < NUM_COLUMNS; j++)
+        for (int i = 0; i < GameConfig.NUM_ROWS; i++) {
+            for (int j = 0; j < GameConfig.NUM_COLUMNS; j++)
                 bgGrid.add(new CellActor(floor));
             bgGrid.row();
         }
@@ -125,7 +118,7 @@ public class GameScreen extends ScreenAdapter {
         return bgTable;
     }
 
-    private Actor createGridMiddle() {
+    private Actor createMiddle() {
         Image mid = new Image(gameplayAtlas.findRegion(AssetRegionNames.MIDDLE));
         mid.setPosition(viewport.getWorldWidth() / 2f - 6f,
                 viewport.getWorldHeight() / 2f - 6f);
@@ -133,131 +126,47 @@ public class GameScreen extends ScreenAdapter {
         return mid;
     }
 
-    private Actor createGrid() {
+    private Actor createGridMain() {
         final Table table = new Table();
-        table.setDebug(false);   // turn on all debug lines (table, cell, and widget)
 
         final Table grid = new Table();
-        grid.defaults().size(CELL_SIZE);   // all cells will be the same size
-        grid.setDebug(false);
-
-        final TextureRegion coneRed = gameplayAtlas.findRegion(AssetRegionNames.CONE_RED);
-
-        final TextureRegion playerBlue = gameplayAtlas.findRegion(AssetRegionNames.PLAYER_BLUE);
-
-        final TextureRegion basketBlue = gameplayAtlas.findRegion(AssetRegionNames.BASKET_BLUE);
-        final TextureRegion basketOrange = gameplayAtlas.findRegion(AssetRegionNames.BASKET_ORANGE);
-
-        final TextureRegion empty = gameplayAtlas.findRegion(AssetRegionNames.EMPTY);
+        grid.defaults().size(GameConfig.CELL_SIZE);   // all cells will be the same size
 
         TextureRegion[] obstacles = {
                 gameplayAtlas.findRegion(AssetRegionNames.POM_PINK),
                 gameplayAtlas.findRegion(AssetRegionNames.POM_YELLOW),
                 gameplayAtlas.findRegion(AssetRegionNames.WHISTLE),
         };
-
         TextureRegion[] fixedObstacles = {
                 gameplayAtlas.findRegion(AssetRegionNames.CONE_BLUE),
                 coneRed
         };
-        List<List<Integer>> drawFixedObstaclesIn = List.of(
-                List.of(1, 11),
-                List.of(10, 15),
-                List.of(10, 7),
-                List.of(10, 8),
-                List.of(10, 9),
-                List.of(11, 11),
-                List.of(11, 13),
-                List.of(11, 3),
-                List.of(11, 5),
-                List.of(12, 10),
-                List.of(12, 11),
-                List.of(12, 13),
-                List.of(12, 14),
-                List.of(12, 2),
-                List.of(12, 3),
-                List.of(12, 5),
-                List.of(12, 6),
-                List.of(12, 8),
-                List.of(13, 5),
-                List.of(2, 10),
-                List.of(2, 11),
-                List.of(2, 13),
-                List.of(2, 14),
-                List.of(2, 2),
-                List.of(2, 3),
-                List.of(2, 5),
-                List.of(2, 6),
-                List.of(2, 8),
-                List.of(3, 11),
-                List.of(3, 13),
-                List.of(3, 3),
-                List.of(3, 5),
-                List.of(4, 1),
-                List.of(4, 7),
-                List.of(4, 8),
-                List.of(4, 9),
-                List.of(5, 11),
-                List.of(5, 13),
-                List.of(5, 3),
-                List.of(5, 5),
-                List.of(6, 11),
-                List.of(6, 13),
-                List.of(6, 14),
-                List.of(6, 2),
-                List.of(6, 3),
-                List.of(6, 5),
-                List.of(8, 11),
-                List.of(8, 13),
-                List.of(8, 14),
-                List.of(8, 2),
-                List.of(8, 3),
-                List.of(8, 5),
-                List.of(9, 11),
-                List.of(9, 13),
-                List.of(9, 3),
-                List.of(9, 5)
-        );
-
-        List<Integer> location;
 
         // init all cells as empty
-        for (int i = 0; i < NUM_ROWS; i++) {
+        for (int i = 0; i < GameConfig.NUM_ROWS; i++) {
             cells.add(new ArrayList<>());
-            for (int j = 0; j < NUM_COLUMNS; j++) {
+            for (int j = 0; j < GameConfig.NUM_COLUMNS; j++) {
                 cells.get(i).add(new CellActor(empty));
             }
         }
 
-        for (int row = 0; row < NUM_ROWS; row++) {
-            for (int column = 0; column < NUM_COLUMNS; column++) {
-                location = List.of(row, column);
+        for (int row = 0; row < GameConfig.NUM_ROWS; row++) {
+            for (int column = 0; column < GameConfig.NUM_COLUMNS; column++) {
+                List<Integer> location = List.of(row, column);
 
-                // draw broder
-                if (row == 0 || row == NUM_ROWS - 1 || column == 0 || column == NUM_COLUMNS - 1) {
+                if (row == 0 || row == GameConfig.NUM_ROWS - 1 || column == 0 || column == GameConfig.NUM_COLUMNS - 1)
                     cells.get(row).get(column).setDrawable(coneRed);
-                }
-                // draw player
-                else if (row == 1 && column == 1) {
+                else if (row == playerPosition.get(0) && column == playerPosition.get(1))
                     cells.get(row).get(column).setDrawable(playerBlue);
-                    playerPosition.add(row);
-                    playerPosition.add(column);
-                }
-                // draw center
-                else if (row == 5 && column == 8) {
+                else if (row == 5 && column == 8)
                     cells.get(row).get(column).setDrawable(basketBlue);
-                } else if (row == 7 && column == 6) {
+                else if (row == 7 && column == 6)
                     cells.get(row).get(column).setDrawable(basketOrange);
-                }
-                // draw fixed obstacles
-                else if (drawFixedObstaclesIn.contains(location)) {
+                else if (GameConfig.LOC_FIXED_OBSTACLES.contains(location))
                     cells.get(row).get(column).setDrawable(fixedObstacles[ThreadLocalRandom.current().nextInt(fixedObstacles.length)]);
-                }
-                // draw destroyable obstacles
-                else if (ThreadLocalRandom.current().nextBoolean()) {
+                else if (ThreadLocalRandom.current().nextBoolean())
                     // TODO: except around player
                     cells.get(row).get(column).setDrawable(obstacles[ThreadLocalRandom.current().nextInt(obstacles.length)]);
-                }
 
                 grid.add(cells.get(row).get(column));
             }
@@ -272,11 +181,10 @@ public class GameScreen extends ScreenAdapter {
         gameplayStage.addListener(new InputListener() {
             @Override
             public boolean keyDown(InputEvent event, int keycode) {
-                if (
-                        keycode == Input.Keys.UP
-                                || keycode == Input.Keys.DOWN
-                                || keycode == Input.Keys.LEFT
-                                || keycode == Input.Keys.RIGHT
+                if (keycode == Input.Keys.UP
+                        || keycode == Input.Keys.DOWN
+                        || keycode == Input.Keys.LEFT
+                        || keycode == Input.Keys.RIGHT
                 ) {
                     Integer rowPlayer = playerPosition.get(0);
                     Integer colPlayer = playerPosition.get(1);
