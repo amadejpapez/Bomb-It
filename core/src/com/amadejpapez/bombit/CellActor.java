c57b3d3c46161737bdbe69c60a6e2a34e874d06a
amadejpapez <amadej.papez@icloud.com>
2023-12-27 16:55:32 +0100

    ref: remove CellState as not used

--- core/src/com/amadejpapez/bombit/CellActor.java
+++ core/src/com/amadejpapez/bombit/CellActor.java
@@ -5,22 +5,11 @@ import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 
 public class CellActor extends Image {
-    private CellState state;
-
     public CellActor(TextureRegion region) {
         super(region);
-        state = CellState.EMPTY;
-    }
-
-    public void setState(CellState state) {
-        this.state = state;
     }
 
     public void setDrawable(TextureRegion region) {
         super.setDrawable(new TextureRegionDrawable(region));
-    }
-
-    public boolean isEmpty() {
-        return state == CellState.EMPTY;
     }
 }
