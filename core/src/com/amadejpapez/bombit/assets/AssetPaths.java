7bbeedd3d384bef72e19e78249bb20a2b6e6ac12
amadejpapez <63184600+amadejpapez@users.noreply.github.com>
2025-11-23 10:22:47 +0100

    fix: asset paths directory prefix

--- core/src/com/amadejpapez/bombit/assets/AssetPaths.java
+++ core/src/com/amadejpapez/bombit/assets/AssetPaths.java
@@ -1,9 +1,9 @@
 package com.amadejpapez.bombit.assets;
 
 public class AssetPaths {
-    public static final String GAMEPLAY = "assets/atlas/gameplay.atlas";
+    public static final String GAMEPLAY = "atlas/gameplay.atlas";
 
-    public static final String FONT = "assets/fonts/arial-32.fnt";
+    public static final String FONT = "fonts/arial-32.fnt";
 
     public static final String SKIN = "skin/neon-ui.json";
 
